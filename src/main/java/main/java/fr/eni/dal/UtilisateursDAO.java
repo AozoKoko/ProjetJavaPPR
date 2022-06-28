6b91eadd55818692d781b87b74d42b1e7df3a01a
probial2022 <probial2022@12SE14-H7LRZS2.ad.campus-eni.fr>
2022-06-28 09:26:42 +0200

    Merge branch 'main' of https://github.com/AozoKoko/ProjetJavaPPR.git into main

--- src/main/java/main/java/fr/eni/dal/UtilisateursDAO.java
+++ src/main/java/main/java/fr/eni/dal/UtilisateursDAO.java
@@ -1,13 +1,10 @@
 package main.java.fr.eni.dal;
 
-import java.util.List;
-
 import main.java.fr.eni.bll.BLLException;
 import main.java.fr.eni.bo.Utilisateur;
 
 public interface UtilisateursDAO {
 	Utilisateur insert (Utilisateur user) throws BLLException;
-	List<Utilisateur> selectAll() throws BLLException;
 	Utilisateur selectById(Integer id) throws DALException, BLLException;
 	Utilisateur verifLogin(String pseudo, String motDePasse) throws DALException, BLLException;
 }
